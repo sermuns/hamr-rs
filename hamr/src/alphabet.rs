718871a88eca30cc6dc243337468f8964f9163b5
Samuel Åkesson <sermuns@lysator.liu.se>
2026-08-14 15:37:51 +0200

    feat(hamr): slowly implementing str_to_number

--- hamr/src/alphabet.rs
+++ hamr/src/alphabet.rs
@@ -1,3 +1,5 @@
+use thiserror::Error;
+
 #[derive(Clone, Default, Debug)]
 #[cfg_attr(feature = "std", derive(clap::ValueEnum))]
 pub enum Alphabet {
@@ -7,5 +9,87 @@ pub enum Alphabet {
     Emoji,
 }
 
-pub const OUTPUT_ALPHABET_ASCII: &str =
-    "!#$&'()*+,-./0123456789:;=?~@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]_abcdefghijklmnopqrstuvwxyz";
+#[derive(Error, Debug, PartialEq)]
+pub enum StrToNumError {
+    #[error("encountered `{0}`")]
+    InvalidCharacter(char),
+}
+
+impl Alphabet {
+    // TODO: maybe don't use `usize`?
+    pub fn str_to_number(&self, input: &str) -> Result<usize, StrToNumError> {
+        let mut number = 0;
+
+        for c in input.chars() {
+            let digit = self
+                .get_slice()
+                .iter()
+                .position(|a| *a == c)
+                .ok_or_else(|| StrToNumError::InvalidCharacter(c))?;
+
+            number *= self.alphabet_size();
+            number += digit + 1;
+        }
+
+        Ok(number)
+    }
+
+    pub const fn get_slice(&self) -> &'static [char] {
+        match self {
+            Self::Ascii => OUTPUT_ALPHABET_ASCII,
+            Self::Qr => OUTPUT_ALPHABET_QR,
+            Self::Emoji => OUTPUT_ALPHABET_EMOJI,
+        }
+    }
+
+    pub const fn alphabet_size(&self) -> usize {
+        self.get_slice().len()
+    }
+}
+
+pub const OUTPUT_ALPHABET_ASCII: &[char] = &[
+    '!', '#', '$', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5',
+    '6', '7', '8', '9', ':', ';', '=', '?', '~', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']',
+    '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+pub const OUTPUT_ALPHABET_QR: &[char] = &[
+    '$', '*', '+', '-', '.', '/', ':', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z',
+];
+// TODO:
+pub const OUTPUT_ALPHABET_EMOJI: &[char] = &[];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+    use std::assert_matches;
+
+    #[rstest]
+    #[case(Alphabet::Ascii, "!", 1)]
+    #[case(Alphabet::Ascii, "A", 30)]
+    #[case(Alphabet::Ascii, "!!", 85)]
+    #[case(Alphabet::Ascii, "!!!", 7141)]
+    fn str_to_number_works(
+        #[case] alphabet: Alphabet,
+        #[case] input: &str,
+        #[case] expected: usize,
+    ) {
+        let result = alphabet.str_to_number(input).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
+    #[case(Alphabet::Qr, "!", StrToNumError::InvalidCharacter('!'))]
+    fn str_to_number_errors(
+        #[case] alphabet: Alphabet,
+        #[case] input: &str,
+        #[case] expected: StrToNumError,
+    ) {
+        let err = alphabet.str_to_number(input).unwrap_err();
+        assert_eq!(err, expected);
+    }
+}
--- hamr/src/decompress.rs
+++ hamr/src/decompress.rs
@@ -2,4 +2,7 @@ use core::fmt::Write;
 
 use crate::alphabet::Alphabet;
 
-pub fn decompress(payload: &str, alphabet: Alphabet, output: impl Write) {}
+/// Decodes and decompresses the payload (writing into `output`) assuming the given alphabet and produces a full link.
+pub fn decompress(payload: &str, alphabet: Alphabet, output: impl Write) {
+    let number = alphabet.str_to_number(payload);
+}
