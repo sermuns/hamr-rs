2575f5e43d1c8361d9e037b9d197f2571dab9f18
Samuel Åkesson <sermuns@lysator.liu.se>
2026-08-17 12:32:52 +0200

    wip(hamr): halfway through compress..

--- hamr/src/compress.rs
+++ hamr/src/compress.rs
@@ -1,12 +1,23 @@
 use core::fmt::Write;
+use std::borrow::Cow;
 
 use thiserror::Error;
 use url::Url;
 
 use crate::{
-    alphabet::Alphabet,
+    alphabet::{Alphabet, SUBALPHABETS},
     huffman::{Lookup, sld::SLD_LIST, tld::TldEncode},
+    segment_type::SegmentType,
 };
+
+pub enum Segment<'a> {
+    Hash(&'a str),
+    Path(&'a str),
+    Query {
+        key: Cow<'a, str>,
+        value: Cow<'a, str>,
+    },
+}
 
 #[derive(Error, Debug)]
 pub enum CompressionError {
@@ -21,7 +32,7 @@ pub fn compress(
     alphabet: Alphabet,
     mut output: impl Write,
 ) -> Result<(), CompressionError> {
-    let number = 1u64;
+    let mut number = 1u64;
 
     // TODO: auto-add 'http://' if needed
     let url = Url::parse(input).map_err(|_| CompressionError::InvalidUrl)?;
@@ -35,7 +46,7 @@ pub fn compress(
         // BUG: untested
         hostname.truncate(last_dot_index);
     }
-    dbg!(hostname);
+    dbg!(&hostname);
 
     let is_https = match url.scheme() {
         "https" => true,
@@ -48,7 +59,89 @@ pub fn compress(
         hostname.drain(..4);
     }
 
-    let known_sld = SLD_LIST.iter().find(todo!());
+    let known_sld: &str = SLD_LIST
+        .iter()
+        .copied()
+        .find(|sld| hostname.ends_with(sld))
+        .unwrap_or("");
+    let subdomain = &hostname[..known_sld.len()];
+
+    // The seperable parts of a path/query are split into segments with
+    // their position/role in the link noted. This lets us pick optimal
+    // character sets for individual segments and enables us to encode
+    // only the transitions between segments. As-is, the system's a bit
+    // clumsy, but it works.
+    let mut path_segments: Vec<_> = url
+        .path_segments()
+        .unwrap()
+        .filter(|s| !s.is_empty()) // FIXME: necessary?
+        .map(|s| Segment::Path(s))
+        .collect();
+
+    let (has_index_html, has_index_php) = match *path_segments.last().unwrap() {
+        Segment::Path("index.html") => {
+            path_segments.pop();
+            (true, false)
+        }
+        Segment::Path("index.php") => {
+            path_segments.pop();
+            (false, true)
+        }
+        _ => (false, false),
+    };
+
+    let query_params = url
+        .query_pairs()
+        .map(|(key, value)| Segment::Query { key, value });
+    path_segments.extend(query_params);
+
+    if let Some(hash) = url.fragment() {
+        path_segments.push(Segment::Hash(hash));
+    }
+
+    // TODO:
+    // Normalize path segment encoding
+
+    // Encode path following domain segment-by-segment, using best algorithm for each
+    let mut last_segment_type = SegmentType::from(path_segments.last().unwrap());
+    let mut query_param_index = 0;
+
+    for (j, segment) in path_segments.iter().enumerate().rev() {
+        let first_iteration = j == path_segments.len() - 1;
+        if !first_iteration && query_param_index % 2 == 0 {
+            // Indicate change of segment type (path -> param -> hash)
+            //   First bit indicates that a change is happening,
+            //   second bit indicates whether we're skipping straight to the hash.
+            number <<= 1;
+            match (&last_segment_type, segment) {
+                (SegmentType::Hash, Segment::Query { .. }) => {
+                    number += 1;
+                }
+                (SegmentType::Hash, Segment::Path { .. }) => {
+                    number += 1; // Second bit is 1
+                    number <<= 1;
+                    number += 1;
+                }
+                _ if last_segment_type != segment.into() => {
+                    // Second bit is 0
+                    number <<= 1;
+                    number += 1;
+                }
+                _ => (),
+            }
+            last_segment_type = segment.into();
+        }
+        if matches!(segment, Segment::Query { .. }) {
+            query_param_index += 1;
+        }
+
+        // Look for smallest subalphabet that fits this path segment
+        let mut subalphabet_index = None;
+        let mut subalphabet = None;
+        for (i, subalphabet) in SUBALPHABETS.iter().enumerate() {
+            if 
+        }
+    }
 
     Ok(())
 }
--- hamr/src/segment_type.rs
+++ hamr/src/segment_type.rs
@@ -1,8 +1,21 @@
 use strum::FromRepr;
 
-#[derive(FromRepr)]
+use crate::compress::Segment;
+
+#[derive(FromRepr, PartialEq)]
 pub enum SegmentType {
     Path,
     Query,
     Hash,
+}
+
+impl From<&Segment<'_>> for SegmentType {
+    // TODO: seems verbose and stupid...
+    fn from(value: &Segment) -> Self {
+        match value {
+            Segment::Query { .. } => SegmentType::Query,
+            Segment::Hash(..) => SegmentType::Hash,
+            Segment::Path(..) => SegmentType::Path,
+        }
+    }
 }
